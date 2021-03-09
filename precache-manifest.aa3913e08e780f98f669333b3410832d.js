44505e976ef98dfb17d95fa7ae9ae0858f9afd3e
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-03-09 13:48:04 -0300

    Updates

---
+++ precache-manifest.aa3913e08e780f98f669333b3410832d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ac1eb3fc5cdfb7232bc723d7cd82e3",
+    "url": "/Frontend_Store_project/index.html"
+  },
+  {
+    "revision": "8fb8646b75aac70982d5",
+    "url": "/Frontend_Store_project/static/css/main.ae1a2eed.chunk.css"
+  },
+  {
+    "revision": "da81a8700d5f77a1eb66",
+    "url": "/Frontend_Store_project/static/js/2.2a7ab3f3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Frontend_Store_project/static/js/2.2a7ab3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb8646b75aac70982d5",
+    "url": "/Frontend_Store_project/static/js/main.ff3e0cce.chunk.js"
+  },
+  {
+    "revision": "065a0740c68cf67cddbd",
+    "url": "/Frontend_Store_project/static/js/runtime-main.4f87440c.js"
+  }
+]);
